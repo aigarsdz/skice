555c6722d103ebe4851a43d72ccbeb4794029317
Aigars Dzerviniks <aigarsdz@pm.me>
2023-11-25 20:06:57 +0200

    Adds a function to import plain text files.

---
+++ assets/network.js
@@ -0,0 +1,12 @@
+export async function importPlainText (url) {
+  try {
+    const response = await fetch(url)
+    const textResponse = await response.text()
+
+    return textResponse
+  } catch (error) {
+    console.error(error)
+
+    return ''
+  }
+}
